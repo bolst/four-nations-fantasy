f35ef0da67026d5a8dc1d65c1544b5f1c8519215
nic <bolton21@uwindsor.ca>
2025-01-13 21:45:50 -0500

    - qol

--- FourNationsFantasy/Data/Data.cs
+++ FourNationsFantasy/Data/Data.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Security.Claims;
 using Dapper;
 using Npgsql;
@@ -143,6 +144,24 @@ public class FNFPlayer : IEquatable<FNFPlayer>
     public string Nationality { get; set; }
 
     public int NhlIdInt => int.Parse(NhlId);
+
+    public string Flag => this.Nationality switch
+    {
+        "CAN" => "\ud83c\udde8\ud83c\udde6",
+        "USA" => "\ud83c\uddfa\ud83c\uddf8",
+        "SWE" => "\ud83c\uddf8\ud83c\uddea",
+        "FIN" => "\ud83c\uddeb\ud83c\uddee",
+        _ => "\ud83c\udf0d",
+    };
+
+    public string FullPosition => this.Position switch
+    {
+        "F" => "Forward",
+        "D" => "Defense",
+        "G" => "Goalie",
+        _ => string.Empty,
+    };
+    
     public override bool Equals(object obj)
     {
         if (obj is null) return false;
