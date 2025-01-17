5bc676a80aabc9642e29d9cd80177175971200e4
nic <bolton21@uwindsor.ca>
2025-01-17 12:21:50 -0500

    - qol

--- FourNationsFantasy/Hubs/DraftHub.cs
+++ FourNationsFantasy/Hubs/DraftHub.cs
@@ -10,16 +10,5 @@ public class DraftHub : Hub
     {
         await Clients.All.SendAsync("DraftPlayer", player, user);
     }
-
-    public override Task OnConnectedAsync()
-    {
-        Console.WriteLine($"{Context.ConnectionId} connected");
-        return base.OnConnectedAsync();
-    }
-
-    public override async Task OnDisconnectedAsync(Exception e)
-    {
-        Console.WriteLine($"Disconnected {e?.Message} {Context.ConnectionId}");
-        await base.OnDisconnectedAsync(e);
-    }
+    
 }
