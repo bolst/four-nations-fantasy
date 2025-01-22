68bc4dda67b6c9bf1e32bf77b1d0752e808ae9a0
nic <bolton21@uwindsor.ca>
2025-01-22 09:43:31 -0500

    - add sorting on draft

--- FourNationsFantasy/Data/Services.cs
+++ FourNationsFantasy/Data/Services.cs
@@ -49,6 +49,7 @@ public class ScoreCalculationService
 {
     private readonly DateOnly FirstDate = new DateOnly(2025, 02, 12);
     private readonly DateOnly LastDate = new DateOnly(2025, 02, 17);
+    private Dictionary<string, double> FantasyPointCache = new();
     
     public const double Goal = 3;
     public const double Assist = 1;
@@ -61,7 +62,7 @@ public class ScoreCalculationService
     public const double GoalieGoalAgainst = -2;
     public const double GoalieSave = 0.2;
     public const double GoalieShutout = 5;
-    
+
     public double CalculatePlayerGameScore(Nhl.Api.Models.Game.PlayerGameLog gameLog)
     {
         return gameLog.Goals * Goal
@@ -81,12 +82,26 @@ public class ScoreCalculationService
 
     public double CalculatePlayerSeasonScore(FNFPlayer player)
     {
-        if (player.Position == "G") return CalculateGoalieSeasonScore(player);
+        if (FantasyPointCache.TryGetValue(player.NhlId, out double score))
+        {
+            return score;
+        }
         
-        return CalculateSkaterSeasonScore(player);
+        if (player.Position == "G")
+        {
+            score = CalculateGoalieSeasonScore(player);
+        }
+        else
+        {
+            score = CalculateSkaterSeasonScore(player);
+        }
+        
+        FantasyPointCache.Add(player.NhlId, score);
+
+        return score;
     }
 
-    public double CalculateSkaterSeasonScore(FNFPlayer player)
+    private double CalculateSkaterSeasonScore(FNFPlayer player)
     {
         return player.Goals * Goal
                + player.Assists * Assist
@@ -97,7 +112,7 @@ public class ScoreCalculationService
                + player.Blocks * Block; 
     }
     
-    public double CalculateGoalieSeasonScore(FNFPlayer player)
+    private double CalculateGoalieSeasonScore(FNFPlayer player)
     {
         return player.GoalieWins * GoalieWin
                + player.GoalieGoalsAgainst * GoalieGoalAgainst
